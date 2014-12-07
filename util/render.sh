13ee529f4996f4c5716c6be533ee26c29c66c8cc
Austin Conner <aconner.vu@gmail.com>
2014-12-07 06:56:03 -0600

    more robust render script, rename top level files to not have spaces

--- util/render.sh
+++ util/render.sh
@@ -1,2 +1,8 @@
-#!/bin/sh
-./toxml.py "$1" | xsltproc graph.xsl - | neato -Txlib
+#/bin/bash
+neatoarg="-Elen=${2:-3}"
+if [ "$3" == "xlib" ]; then
+  neatoarg="$neatoarg -Txlib"
+else
+  neatoarg="$neatoarg -Tpng -o $(basename -s.tex $1).png"
+fi
+./toxml.py "$1" | xsltproc graph.xsl - | neato $neatoarg
