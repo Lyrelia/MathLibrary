70e245f0409de9689d66692811784dd07ea9a47c
Austin Conner <aconner.vu@gmail.com>
2014-12-06 20:31:04 -0600

    toxml works

--- util/toxml.py
+++ util/toxml.py
@@ -5,30 +5,29 @@ import re
 import sys
 
 def main():
-    tabular=False
-    colheader=[]
-    table = ET.Element('table')
-    for line in open(sys.argv[1]):
-        if '\\begin{tabular}' in line:
-            tabular=True
-        if '\\end{tabular}' in line:
-            tabular=False
-        if tabular and '\\tabularnewline' in line:
-            if colheader == []:
-                colheader = [re.sub(' ','_',c.strip().lower()) 
-                        for c in line.rsplit('\\tabularnewline',1)[0].split('&')]
-                print(colheader)
-            else:
-                row = ET.SubElement(table,'row')
-                cols = [c.strip() for c in line.rsplit('\\tabularnewline',1)[0].split('&')]
-                print(cols)
-                for i in range(len(colheader)):
-                    c = ET.SubElement(row,colheader[i])
-                    c.text=cols[i]
+    xml = readfile(sys.argv[1])
+    print(ET.tostring(xml).decode())
 
-    print(ET.tostring(table))
+def readfile(fname):
+    text=open(fname).read()
+    text=text.split('\\begin{tabular}',1)[1]
+    text=text.split('}',1)[1]
+    text=text.rsplit('\\end{tabular}',1)[0]
+    text=re.sub('\\\\hline','',text)
+    rows=text.split('\\tabularnewline')[:-1]
+    mat=[[c.strip() for c in r.split('&')] for r in rows]
 
+    colheader=[re.sub(' ','_',c.lower()) for c in mat[0]]
+    mat=mat[1:]
 
+    graph=ET.Element('graph')
+    for row in mat:
+        r=ET.SubElement(graph,'node')
+        for i in range(len(colheader)):
+            e=ET.SubElement(r,colheader[i])
+            e.text=row[i]
+
+    return graph
 
 if __name__ == "__main__":
     main()
